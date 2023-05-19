74a679918b08fae906f809bfd6e3467fedb23ae5
Xi Chen <xichen6@microsoft.com>
2023-05-19 18:14:08 +0800

    fix error of uploading files

--- accessing_request_data/uploading_files/app.py
+++ accessing_request_data/uploading_files/app.py
@@ -5,11 +5,14 @@ from werkzeug.utils import secure_filename
 import os
 
 # where to store the uploaded files
-UPLOAD_FOLDER = r'.\data\uploaded_files'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(current_dir, 'data', 'uploaded_files')
 ALLOW_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Set the secret key to some random bytes. Keep this really secret!
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 def allow_file(filename):
     return '.' in filename and \
@@ -17,21 +20,29 @@ def allow_file(filename):
         
 @app.route('/', methods=['POST', 'GET'])
 def upload_file():
+    app.logger.debug('entering upload file view.')
     if request.method == 'POST':
         # check if the post request contain a file named 'file'
         if 'file' not in request.files:
+            app.logger.debug('option 1')
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
         
         # if user does not select a file, the browser submit a empty file without a filename(no select file and submit)
         if file.filename == '':
+            app.logger.debug('option 2')
             flash('No file selected')
             return redirect(request.url)
         
         if file and allow_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            if not os.path.isdir(UPLOAD_FOLDER):
+                os.makedirs(UPLOAD_FOLDER)
+            save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            app.logger.debug(f'saving to {save_path}')
+            file.save(save_path)
+            app.logger.debug(f'saved')
             return redirect(url_for('download_file', filename=filename))
         
     return render_template('index.html')
